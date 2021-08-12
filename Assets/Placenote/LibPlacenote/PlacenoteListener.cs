4fd7e31bed6dcff265acdb8233e2dce76c95393b
hyoGD <trungghieuGD@gmail.com>
2021-08-12 20:11:00 +0700

    ss
    ds

--- Assets/Placenote/LibPlacenote/PlacenoteListener.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface PlacenoteListener
-{
-    void OnPose(Matrix4x4 outputPose, Matrix4x4 arkitPose);
-    void OnStatusChange(LibPlacenote.MappingStatus prevStatus, LibPlacenote.MappingStatus currStatus);
-    void OnLocalized();
-}
